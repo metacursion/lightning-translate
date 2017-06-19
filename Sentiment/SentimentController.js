dc76f904b27fb73365d74a9e763a1e8c3fe1f74e
Jurgis <heyoo@bayesian-goat.lan>
2017-06-19 22:22:10 +1200

    add gif

--- Sentiment/SentimentController.js
+++ Sentiment/SentimentController.js
@@ -1,15 +1,18 @@
 ({
-    getRes : function(component, event, helper) {
-        const key = 'AIzaSyDGTM7TBaq-8g7Ey93AUDnLUBvjlrlzAuE'
-        const api = 'https://translation.googleapis.com/language/translate/v2?key='
-        const query = event.getParam("value")
-        const body = JSON.stringify( {q: query, target: 'en' })
-        fetch(api+key, {method: 'POST', body:body})
-        .then( m => m.json() )
-        .then( j => {
-            console.log(j)
-            if( j && j.data && j.data.translations )
-            	component.set('v.output', j.data.translations[0].translatedText)
-        })
-        }
-        })
+  getRes: function(component, event, helper) {
+    const key = 'AIzaSyDGTM7TBaq-8g7Ey93AUDnLUBvjlrlzAuE'
+    const api = 'https://translation.googleapis.com/language/translate/v2?key='
+    const query = event.getParam("value")
+    const body = JSON.stringify({
+      q: query,
+      target: 'en'
+    })
+    fetch(api + key, {
+      method: 'POST',
+      body: body
+    }).then(m => m.json()).then(j => {
+      console.log(j)
+      if (j && j.data && j.data.translations) component.set('v.output', j.data.translations[0].translatedText)
+    })
+  }
+})
